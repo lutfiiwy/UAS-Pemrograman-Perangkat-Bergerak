302f10126c963dacee9118b7a533bcd93c96a048
lutfiiwy <73923353+lutfiiwy@users.noreply.github.com>
2022-02-15 14:48:09 +0700

    Add files via upload

---
+++ NavDrawer_Lutfi Indiarto Wirayuda/app/src/main/java/lutfiiwy/platform/navdrawer/IbuAnak.kt
@@ -0,0 +1,11 @@
+package lutfiiwy.platform.navdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IbuAnak : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ibu_anak)
+    }
+}
---
+++ NavDrawer_Lutfi Indiarto Wirayuda/app/src/main/java/lutfiiwy/platform/navdrawer/Rumah.kt
@@ -0,0 +1,11 @@
+package lutfiiwy.platform.navdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Rumah : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rumah)
+    }
+}
